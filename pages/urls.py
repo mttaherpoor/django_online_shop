b8b28bb6930acab79eef3c356643c355808d49ec
Taher <131670439+mttaherpoor@users.noreply.github.com>
2025-03-10 21:06:57 +0330

    add test for pages

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('aboutus', views.AboutUSView.as_view(), name='aboutus'),
+    path('aboutus/', views.AboutUSView.as_view(), name='aboutus'),
 
 ]
