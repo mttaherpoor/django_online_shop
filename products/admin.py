eb0c05882d94c321c7935b13294d6d636fc72cb5
Taher <131670439+mttaherpoor@users.noreply.github.com>
2025-03-15 02:18:37 +0330

    add tabular inline for product

--- products/admin.py
+++ products/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 
 from .models import Product,Comment
 
+
+class CommentInLine(admin.TabularInline): #admin.StackedInline
+    model = Comment
+    fields = ['auther', 'body', 'stars', 'active', ]
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', ]
+    inlines =[
+        CommentInLine,
+    ]
 
 
 @admin.register(Comment)
