5a48254c1ae82be0f07c364140e3aa606d3c4a03
Taher <131670439+mttaherpoor@users.noreply.github.com>
2025-03-15 02:19:41 +0330

    add custom templatetags for detailview

---
+++ products/templatetags/commnet_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from django.db.models import QuerySet
+
+from ..models import Comment
+
+register = template.Library()
+
+@register.filter
+def only_active_comments(comments: QuerySet[Comment]) -> QuerySet[Comment]:
+    return comments.filter(active=True) #comments.exclude(active=False)
+ 
